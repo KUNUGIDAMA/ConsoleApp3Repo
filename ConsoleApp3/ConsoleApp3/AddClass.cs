28ea0dc94f5f93124e42579dc3c8790b3b797f53
KUNUGI <kokoro.tera1945@gmail.com>
2020-09-30 10:55:52 +0900

    addFile:AddClass.cs

---
+++ ConsoleApp3/ConsoleApp3/AddClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class AddClass
+    {
+        public int memverCode { get; set; }
+        public string memberName { get; set; }
+
+        AddClass()
+        {
+            memverCode = 0;
+            memberName = "Empty";
+        }
+
+    }
+}
